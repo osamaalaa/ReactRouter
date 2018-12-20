e0128a2a32026722cbd707f14538df1fe17340a5
osama alaa <osamaalaa133@gmail.com>
2018-12-20 15:30:05 +0200

    ReactRouter
    web Application with React , this project focused on React Router and how to deal with many routes and how to do URL params and dynamic routing

---
+++ src/components/Lost.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Lost = () => {
+  return (
+    <div>
+      <h1 style={{color: "white"}}>
+        You`re lost! Get Back...now!!
+      </h1>
+      <img style={{paddingTop: "50px"}} src="/imgs/biohazard.png" alt="Back image"/>  
+    </div>
+  )
+
+
+
+}
+export default Lost ;
